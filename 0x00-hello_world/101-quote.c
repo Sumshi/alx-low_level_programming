3030f77cacad936a9c0fb7bd9319d7b09a15904c
Sumshi <sumayasomow@gmail.com>
2023-03-16 08:08:48 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
+ *main - Entry point
+ *Return: Always 1
  *
- * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long l = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
-__asm__ ("syscall"
-: "=a" (ret)
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (l));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 
 }
+
