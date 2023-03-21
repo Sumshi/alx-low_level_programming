eb426bf4d2b51ec594b4bfa81ca011e614447db8
Sumshi <sumayasomow@gmail.com>
2023-03-21 18:10:11 +0000

    prints the sum of all the multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+int i;
+int sum;
+
+sum = 0;
+
+for (i = 0 ; i < 1024 ; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+sum = sum + i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
+
+
+
