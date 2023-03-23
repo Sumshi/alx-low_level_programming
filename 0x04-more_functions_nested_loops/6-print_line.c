c6f9080b15aef0d77b41706eaf3e67b7daaba6e8
Sumshi <sumayasomow@gmail.com>
2023-03-23 12:37:54 +0000

    draws a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,15 +13,16 @@ void print_line(int n)
 	{
 		_putchar('\n');
 	}
-	
 	else
 
 	{
 
-	for (i = 0 ; i <= n ; i++)
+		for (i = 1 ; i <= n ; i++)
+
 		{
 			_putchar('_');
 		}
+
 		_putchar('\n');
 	}
 }
