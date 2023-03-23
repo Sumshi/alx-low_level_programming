f7912013c496fc26a4fa5f2957e9c82a2ba5eadf
Sumshi <sumayasomow@gmail.com>
2023-03-23 10:37:58 +0000

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers (0 to 9)
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char c;/*defined in putchar function*/
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
