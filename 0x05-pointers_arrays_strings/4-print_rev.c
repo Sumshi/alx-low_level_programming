cb03bdc2093042d13ce1da09ee020cfbe6d044e2
Sumshi <sumayasomow@gmail.com>
2023-03-27 05:43:44 +0000

    prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 	int length = 0;/*stores the length of the string*/
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
+
 		length++;
-	}
+
 	for (i = length; i >= 0; i--)/*decrements the length hence reverse order*/
-	{
+
 		_putchar(s[i]);
-	}
+
 	_putchar('\n');
 }
