87a26fe50f14ba6364fbbb9e31bc10f5789f0b7b
Sumshi <sumayasomow@gmail.com>
2023-03-27 05:39:00 +0000

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: function parameter
+ */
+void print_rev(char *s)
+{
+	/*first we need to know the length of the string*/
+	int i;/*for iterating the string*/
+	int length = 0;/*stores the length of the string*/
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	for (i = length; i >= 0; i--)/*decrements the length hence reverse order*/
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
