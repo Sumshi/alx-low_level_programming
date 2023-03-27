75f53c5dc79176c817bb66b26c701a12881ea75a
Sumshi <sumayasomow@gmail.com>
2023-03-27 07:03:36 +0000

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: parameter to be checked
+ * _putchar - prints each character
+ */
+void rev_string(char *s)
+{
+	int a = 0, b, c;
+	char d;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
+}
