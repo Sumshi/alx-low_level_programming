2ef7b5fb38ef46247020fde234dbdd6c4cbc5b50
Sumshi <sumayasomow@gmail.com>
2023-03-29 15:11:19 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;/*for iterating*/
-
-	/*length of the string differs*/
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		if (s1[i] != s2[i])/*if equal moves to next*/
-		{
-			if (s1[i] < s2[i])
-				return (s1[i] - s2[i]);
-			else if (s1[i] > s2[i])
-				return (s1[i] - s2[i]);
-		}
-		else
-			return (0);/*satisfies when s1[i] = s2[i]*/
+		s1++;
+		s2++;
 	}
-	return (0);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
