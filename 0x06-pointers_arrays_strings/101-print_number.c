e1da80ef54180b60862d43bdcf8cf13d3da867c2
Sumshi <sumayasomow@gmail.com>
2023-03-29 20:35:26 +0000

    prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,20 @@
  */
 void print_number(int n)
 {
-	if (n < 0)/*negative number*/
+	unsigned int x;
+
+	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;/*converted to positive*/
+		n *= -1;
 	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
 /*This checks if the input integer is negative*/
 /*If it is, the '-' character is printed using _putchar function*/
