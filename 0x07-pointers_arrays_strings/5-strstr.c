ea143ac6feefcd0b881c4dd066ba575636b02b07
Sumshi <sumayasomow@gmail.com>
2023-04-03 16:09:38 +0000

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,3 +8,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	for (; *haystack != '\0'; haystack++)
+
+	{
+		char *m = haystack;
+		char *n = needle;
+
+		while (*m == *n && *n != '\0')
+		{
+			m++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+	}
+	return (0);
+}
