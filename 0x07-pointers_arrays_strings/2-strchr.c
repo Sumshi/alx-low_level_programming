18269e5719649eb35c7a4a4e9c21d6bd81923b21
Sumshi <sumayasomow@gmail.com>
2023-04-03 10:11:52 +0000

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
-/*&s[i], the function is returning a pointer to the memory*/
+/*(s + i) the function is returning a pointer to the memory*/
 /*location of the first occurrence of the character*/
 		}
 	}
