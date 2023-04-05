fda4df6fecd7bd782fa8ef1631a4c3139fcddc8d
Sumshi <sumayasomow@gmail.com>
2023-04-05 09:02:44 +0000

    prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _print_rev_recursion - prints a string in reverse
- *@s: parameter
+ *@s: string parameter
  * Return: Always 0.
  */
-	void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {	if (*s)
 	{
 		_print_rev_recursion(s + 1);/*calls itself in reverse*/
 		_putchar(*s);
+/*s + 1, moves pointer to the next character*/
 	}
 }
