e72436ba7da4c2c96846c681dbeb6882005b090b
Sumshi <sumayasomow@gmail.com>
2023-04-07 10:08:21 +0000

    static library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - returns the first occurence of a character
+ *@s: pointer
+ *@c: character to be located
+ * Return: returns s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+			/*(s + i) the function is returning a pointer to the memory*/
+			/*location of the first occurrence of the character*/
+		}
+	}
+	return (NULL);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring.
+ *@haystack: pointer to the string to search
+ *@needle: substring to search for
+ * Return: pointer to the beginning of the located substring
+ *or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;/*iterates for haystack*/
+	int j;/*iterates for needle*/
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+
+		for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++)
+		{
+			/*characters of haystack and needle at the current indices are equal*/
+		}
+
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
+	return (NULL);
+}
