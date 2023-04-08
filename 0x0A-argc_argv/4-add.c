e9d280bd5bde4c35bc81e653fcfe224c371d356a
Sumshi <sumayasomow@gmail.com>
2023-04-08 11:58:40 +0000

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>/*atoi function library*/
+/**
+ * main - multiplies 2 numbers
+ *@argc: number of arguments
+ *@argv: array size of argc
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{/*atoi function converts a string to integer*/
+	int i;/*iterates over command arguments*/
+	int j;/*iterates over characters*/
+	int sum = 0;/*hold sum of numbers*/
+
+	for (i = 1; i < argc; i++)/*we start with 1 cz 0 holds program name*/
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')/*means not digit*/
+			{
+				printf("Error\n");
+				return (1);
+
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
