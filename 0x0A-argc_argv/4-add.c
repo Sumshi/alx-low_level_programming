d692f4ddeb0b9c207acae687c5ad449a8252102b
Sumshi <sumayasomow@gmail.com>
2023-04-10 07:24:45 -0700

    adding positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<ctype.h>
 #include<stdlib.h>/*atoi function library*/
 /**
  * main - adds positive numbers.
@@ -8,17 +7,21 @@
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
-{
-	int i;
-	int sum = 0;
+{/*atoi function converts a string to integer*/
+	int i;/*iterates over command arguments*/
+	int j;/*iterates over characters*/
+	int sum = 0;/*hold sum of numbers*/
 
-	for (i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)/*we start with 1 cz 0 holds program name*/
 	{
-		if (!isdigit(*argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')/*means not digit*/
+			{
+				printf("Error\n");
+				return (1);
 
+			}
 		}
 		sum = sum + atoi(argv[i]);
 	}
