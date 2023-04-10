0166c1bdbb665fa58e364fe8cacf9576f5899692
Sumshi <sumayasomow@gmail.com>
2023-04-10 07:16:50 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,18 +12,16 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
-		for (i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
 		{
-			if (!isdigit(*argv[i]))
-			{
-				printf("Error\n");
-				return (1);
+			printf("Error\n");
+			return (1);
 
-			}
-
-
-			sum = sum + atoi(argv[i]);
 		}
-		printf("%d\n", sum);
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
