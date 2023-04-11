7a27e68fef19b3a164ae9a901627d3918573c937
Sumshi <sumayasomow@gmail.com>
2023-04-11 09:22:41 +0000

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+#include<string.h>
+#include<stdlib.h>
+/**
+ *_strdup - duplicates a string
+ *@str: function parameter
+ *Return: pointer to the duplicated string
+ */
+char *_strdup(char *str);
+{
+	char *new_str;/* stores copied string*/
+	int length;/*stores string length*/
+	int i;
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (length = 0; str[length] != '\0'; str++)/*increments tsring length*/
+
+		new_str = malloc((length * sizeof(char)) + 1)
+			/*plus 1 for the null terminator*/
+			for (i = 0; str[i] != '\0'; i++)
+			{
+				new_str[i] = str[i];/*copies str string*/
+			}
+	new_str[i] = '\0';
+	return (new_str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,8 +2,9 @@
 #ifndef MAIN_H
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
