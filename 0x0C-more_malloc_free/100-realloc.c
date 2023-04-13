09c65bc5f4e6d7f79a1bc229b0c6e916cf059e38
Sumshi <sumayasomow@gmail.com>
2023-04-13 15:07:33 +0000

    reallocates a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,17 +10,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;/*new memory*/
 
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	if (ptr == NULL)
+	if (new_size == old_size)
 	{
-		return (malloc(new_size));
+		return (ptr);
 	}
-
 	if (new_size <= old_size)
 	{
 		return (ptr);/*returns pointer to previous memory*/
