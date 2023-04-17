74e03c7ce7adf9d25c431a6a6e3e97fd569f417f
Sumshi <sumayasomow@gmail.com>
2023-04-17 14:57:53 +0000

    initialize a variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)/*no need to return null since function is void*/
-		d = malloc(sizeof(struct dog));/*a pointer to struct dog*/
-	d->name = name;/*or  (*d).name = name*/
-	d->age = age;/* (*d).age = age*/
-	d->owner = owner;/*(*d).owner = owner*/
+	if (d != NULL)/*no need to return null since function is void*/
+	{
+		d->name = name;/*or  (*d).name = name*/
+		d->age = age;/* (*d).age = age*/
+		d->owner = owner;/*(*d).owner = owner*/
+	}
 }
