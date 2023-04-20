1590c8c3d3ef68f6d2808d526d34936402e2a3d1
Sumshi <sumayasomow@gmail.com>
2023-04-20 18:06:13 +0000

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
 void print_all(const char * const format, ...)
 {
 	int i;
-	int flag;
+	int comma;
 	char *str;
 	va_list a_list;
 
@@ -19,28 +19,28 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(a_list, int));
-				flag = 0;
+				comma = 1;/*comma true*/
 				break;
 			case 'i':
 				printf("%i", va_arg(a_list, int));
-				flag = 0;
+				comma = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(a_list, double));
-				flag = 0;
+				comma = 1;
 				break;
 			case 's':
 				str = va_arg(a_list, char*);
 				if (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
-				flag = 0;
+				comma = 1;
 				break;
 			default:
-				flag = 1;
+				comma = 0;/*no comma*/
 				break;
 		}
-		if (format[i + 1] != '\0' && flag == 0)
+		if (format[i + 1] != '\0' && comma == 1)
 			printf(", ");
 		i++;
 	}
