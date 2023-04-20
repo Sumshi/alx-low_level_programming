2d85b28a0506b2bcb54648dfb4863e71728b3957
Sumshi <sumayasomow@gmail.com>
2023-04-20 09:42:28 +0000

    prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers to print
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, unsigned int));
+		if (i < n - 1)/*check if the current integer is the last one*/
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list);
+}
