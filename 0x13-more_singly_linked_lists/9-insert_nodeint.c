d0d6377d1b98e09495aeee6433d075e3686dfa14
Sumshi <sumayasomow@gmail.com>
2023-04-30 10:39:27 +0000

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,34 +9,34 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;/*pointer to the new node*/
-	listint_t *previous;/*node before the new node*/
-	listint_t *current;/*pointer at the given index*/
+	listint_t *previous_node;/*node before the new node*/
+	listint_t *current_node;/*pointer at the given index*/
 	unsigned int i;/*for looping*/
 
 	if (head == NULL)/*indicates insertion failed*/
 		return (NULL);
-
+	/*if index = 0*/
 	if (idx == 0)/*new node is created*/
 	{
 		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
 			return (NULL);
 
-		new_node->n = n;
+		new_node->n = n;/*n value is assigned to new node*/
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-
-	previous = NULL;
-	current = *head;
-	for (i = 0; current != NULL && i < idx; i++)
+	/*index is not 0*/
+	previous_node = NULL;
+	current_node = *head;
+	for (i = 0; current_node != NULL && i < idx; i++)
 	{
-		previous = current;
-		current = current->next;
+		previous_node = current_node;
+		current_node = current_node->next;
 	}
 
-	if (i < idx)
+	if (i < idx)/*loop terminates b4 reaching given index*/
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -44,7 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = current;
-	previous->next = new_node;
+	new_node->next = current_node;
+	previous_node->next = new_node;
 	return (new_node);
 }
