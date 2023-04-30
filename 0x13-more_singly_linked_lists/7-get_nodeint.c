c67aa3805a981cb63cc980aabf08041d08ff081d
Sumshi <sumayasomow@gmail.com>
2023-04-30 10:01:43 +0000

    returns the nth node of a listint_t

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,8 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
-		if (head == NULL)
-			return (NULL);
+		/*if (head == NULL)
+			return (NULL);*/
 	}
 	return (head);
 }
