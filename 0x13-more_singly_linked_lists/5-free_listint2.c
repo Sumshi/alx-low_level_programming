41f92a89f15f7f6845e73b7478d1ba5a9b11029f
Sumshi <sumayasomow@gmail.com>
2023-05-02 19:06:08 +0000

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current_node;/*keeps track of nodes we want to free*/
 
-	while (*head)
+	while (*head != NULL)
 	{
 		current_node = *head;
 		*head = (*head)->next;/*points to the next node*/
