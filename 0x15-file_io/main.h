511baf4d32f301d67878af6fe44e6683cd843267
Sumshi <sumayasomow@gmail.com>
2023-05-08 14:23:48 +0000

    copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include<string.h>
 #include <fcntl.h>
+#include <errno.h>
+#define BUFFER_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
