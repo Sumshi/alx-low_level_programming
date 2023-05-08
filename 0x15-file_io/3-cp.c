bc7eef2bd5cb47b1c56a9b7a968bbbc8be9fa694
Sumshi <sumayasomow@gmail.com>
2023-05-08 15:04:04 +0000

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#define BUFFER_SIZE 1024
 /**
  * main - program execution begins
  * @argc: number of arguments
- * @argv: array size of argc
- * Return: Always 0
+ *@argv: array size of argc
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int file_to, file_from, bytes_read, bytes_written;
+	int file_to, file_from;
+	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
@@ -16,13 +18,13 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	if (file_from == -1)/*cannot read file*/
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		exit(98);/*read errror*/
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file_to == -1)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)/*cannot write to the file*/
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -33,18 +35,23 @@ int main(int argc, char *argv[])
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			exit(99);/*exit with code 99 for write error*/
 		}
 	}
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		exit(98);/*exit with code 98 for read error*/
 	}
-	if (close(file_from) == -1 || close(file_to) == -1)
+	if (close(file_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);/*exit with code 100 for file descriptor error*/
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);/*exit with code 100 for file descriptor error*/
 	}
 	return (0);
 }
