9b79a46ee06535b54031f0808f7dcb5aa3518144
Sumshi <sumayasomow@gmail.com>
2023-06-15 09:00:53 +0000

    fixed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);/*memory allocation failed*/
 	}
 	new_node->n = n;/*assigns data to the new_node*/
+	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
 
