4a35af3e3c55e3f0f065a3a9e40d987b6413ec53
Sumshi <sumayasomow@gmail.com>
2023-06-15 16:02:28 +0000

    adds node at an index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,30 +11,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-        dlistint_t *tmp = *h, *new;
+	dlistint_t *tmp = *h, *new;
 	unsigned int i;
 
-        if (idx == 0)
-                return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-        for (i = 0; i < idx - 1; i++)
-        {
-                tmp = tmp->next;
-                if (tmp == NULL)
-                        return (NULL);/*index is out of range*/
-        }
-        if (tmp->next == NULL)/*tmp is the last node in the list*/
-                return (add_dnodeint_end(h, n));/*adds node at the end*/
+	for (i = 0; i < idx - 1; i++)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);/*index is out of range*/
+	}
+	if (tmp->next == NULL)/*tmp is the last node in the list*/
+		return (add_dnodeint_end(h, n));/*adds node at the end*/
 
-        new = malloc(sizeof(dlistint_t));
-        if (new == NULL)
-                return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-        new->n = n;/*assigns value to new node*/
-        new->prev = tmp;
-        new->next = tmp->next;
-        tmp->next->prev = new;
-        tmp->next = new;
+	new->n = n;/*assigns value to new node*/
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-        return (new);
+	return (new);
 }
