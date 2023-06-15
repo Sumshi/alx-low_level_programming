58ba83da6c0bbc4e5259b10d6651ed07899c4a97
Sumshi <sumayasomow@gmail.com>
2023-06-15 16:21:33 +0000

    deletes a node at a given index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,28 +11,29 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp = *head;
+	dlistint_t *tmp = *head;/*assigns to head to traverse a list*/
 
-	if (*head == NULL)
+	if (*head == NULL)/*list is empty*/
 		return (-1);
-	for (; index != 0; index--)
+	while (index != 0)
 	{
-		if (tmp == NULL)
+		if (tmp == NULL)/*desired index is out of range*/
 			return (-1);
 		tmp = tmp->next;
+		index--;/*loops till index = 0*/
 	}
-	if (tmp == *head)
+	if (tmp == *head)/*if node to be deleted is head*/
 	{
 		*head = tmp->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
 	}
-	else
+	else/*node to be deleted is not head*/
 	{
 		tmp->prev->next = tmp->next;
 		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
 	}
 	free(tmp);
-	return (1);
+	return (1);/*node deleted succesfully*/
 }
