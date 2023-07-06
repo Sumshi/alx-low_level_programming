f18feee8d049b61299f989db1025f3f5b67105cb
Sumshi <sumayasomow@gmail.com>
2023-07-06 10:13:37 +0000

    calculates hash value

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,12 +7,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_value;
-	unsigned long int index;
-
-	/*call hash_djb2 to calculate hash value for given key*/
-	hash_value = hash_djb2(key);
-	/*calculate the index at which key value should be stored*/
-	index = hash_value % size;
-	return (index);
+	return (hash_djb2(key) % size);
 }
