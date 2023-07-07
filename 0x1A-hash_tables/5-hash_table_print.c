48e8d594d21e23d8a334415504d735ea50d97447
Sumshi <sumayasomow@gmail.com>
2023-07-07 14:57:51 +0000

    5-prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,9 @@
-#include "hash_tables.h"i
-
+#include "hash_tables.h"
 /**
  * hash_table_print - prints a hash table
  * @ht: the hash table to print
  * print the key/value in the order that they appear in the array of hash table
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp_node = NULL;
